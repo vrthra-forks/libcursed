994d3a0bf1cfc753bfe66809907b1ba33cf8c211
xaizek <xaizek@posteo.net>
2019-03-16 16:47:43 +0200

    Add Expander widget
    Empty widget that takes up as much space as it can.

---
+++ Expander.hpp
@@ -0,0 +1,41 @@
+// libcursed -- C++ classes for dealing with curses
+// Copyright (C) 2019 xaizek <xaizek@posteo.net>
+//
+// This file is part of libcursed.
+//
+// libcursed is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// libcursed is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with libcursed.  If not, see <https://www.gnu.org/licenses/>.
+
+#ifndef LIBCURSED__EXPANDER_HPP__
+#define LIBCURSED__EXPANDER_HPP__
+
+#include "Widget.hpp"
+
+namespace cursed {
+
+// Empty widget that takes up as much space as it can.
+class Expander : public Widget
+{
+private:
+    // Updates state of this widget to be published on the screen.
+    virtual void draw() override;
+
+    // Retrieves vertical size policy.
+    // Positive number or zero means exactly that much.
+    // Negative number means at least that much in magnitude.
+    virtual int desiredHeight() override;
+};
+
+}
+
+#endif // LIBCURSED__EXPANDER_HPP__
