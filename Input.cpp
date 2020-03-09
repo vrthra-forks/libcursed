bd664dd1ef3ed2fd2097e7c1b45d4c5cc29d9fe7
xaizek <xaizek@posteo.net>
2020-03-09 15:05:06 +0200

    Disable delay after escape if possible

--- Input.cpp
+++ Input.cpp
@@ -61,6 +61,14 @@ InputElement::operator==(wchar_t wch) const
 
 Input::Input(Keypad keypad)
 {
+#if defined(NCURSES_EXT_FUNCS) && NCURSES_EXT_FUNCS >= 20081102
+    // Use ncurses specific function to make delay after pressing escape key
+    // unnoticeable.  Can be zero, but in some corner cases multiple bytes
+    // composing a functional key code might be handled to the application with
+    // a delay, so small delay is safer.
+    set_escdelay(5);
+#endif
+
     guts::keypad(readWin, keypad == Keypad::Enabled);
     wtimeout(peekWin, 0);
 }
