4c4512cb5dfaf78e61d841bbd06342afae7d5989
xaizek <xaizek@posteo.net>
2020-03-12 13:57:32 +0200

    Fix license comments in three files

--- ListLike.cpp
+++ ListLike.cpp
@@ -1,19 +1,20 @@
+// libcursed -- C++ classes for dealing with curses
 // Copyright (C) 2018 xaizek <xaizek@posteo.net>
 //
-// This file is part of zograscope.
+// This file is part of libcursed.
 //
-// zograscope is free software: you can redistribute it and/or modify
-// it under the terms of the GNU Affero General Public License as published by
+// libcursed is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 //
-// zograscope is distributed in the hope that it will be useful,
+// libcursed is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU Affero General Public License for more details.
+// GNU General Public License for more details.
 //
-// You should have received a copy of the GNU Affero General Public License
-// along with zograscope.  If not, see <http://www.gnu.org/licenses/>.
+// You should have received a copy of the GNU General Public License
+// along with libcursed.  If not, see <https://www.gnu.org/licenses/>.
 
 #include "ListLike.hpp"
 
--- Table.hpp
+++ Table.hpp
@@ -1,19 +1,20 @@
+// libcursed -- C++ classes for dealing with curses
 // Copyright (C) 2016 xaizek <xaizek@posteo.net>
 //
-// This file is part of uncov.
+// This file is part of libcursed.
 //
-// uncov is free software: you can redistribute it and/or modify
-// it under the terms of the GNU Affero General Public License as published by
+// libcursed is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
 //
-// uncov is distributed in the hope that it will be useful,
+// libcursed is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU Affero General Public License for more details.
+// GNU General Public License for more details.
 //
-// You should have received a copy of the GNU Affero General Public License
-// along with uncov.  If not, see <http://www.gnu.org/licenses/>.
+// You should have received a copy of the GNU General Public License
+// along with libcursed.  If not, see <https://www.gnu.org/licenses/>.
 
 #ifndef LIBCURSED__TABLE_HPP__
 #define LIBCURSED__TABLE_HPP__
